bcb291a976d568bbdd52ec7181763d826f2323e0
Madeline Chinarro <173375020+maddieChinarro@users.noreply.github.com>
2025-10-22 09:59:51 +0200

    fix: fixed bug when clicking new cards too fast

--- assets/scripts/memory.js
+++ assets/scripts/memory.js
@@ -6,31 +6,38 @@ export function memory () {
     let cardCpt = 0;
     let victoryCpt = 0;
     let score = 0;
-    let timeOutID;
+    let isTimeOutDone = true;
     cards.forEach((card) => {
         card.addEventListener("click", () => {
-            card.classList.toggle("clicked"); // Display the other side of the card
-            card.style = "pointer-events: none"; // Disable the click-ability
-            if (cardCpt == 1) {
-                if (getSrc(card) != getSrc(storedCard)) { // Unless the previous card's link is equal to the new one's, flip them both back after 1sc
-                    timeOutID = setTimeout(() => {
-                        card.classList.toggle("clicked");
-                        card.style = "pointer-events: auto";
-                        storedCard.classList.toggle("clicked");
-                        storedCard.style = "pointer-events: auto";
-                    }, 1000);
-                } else {
-                    victoryCpt++;
-                    if (victoryCpt == 6) {
-                        alert("Bravo !");
+            if (isTimeOutDone) {
+                card.classList.toggle("clicked"); // Display the other side of the card
+                card.style = "pointer-events: none"; // Disable the click-ability
+                if (cardCpt == 1) {
+                    isTimeOutDone = false;
+                    if (getSrc(card) != getSrc(storedCard)) { // Unless the previous card's link is equal to the new one's, flip them both back after 1sc
+                        setTimeout(() => {
+                            card.classList.toggle("clicked");
+                            card.style = "pointer-events: auto";
+                            storedCard.classList.toggle("clicked");
+                            storedCard.style = "pointer-events: auto";
+                            isTimeOutDone = true;
+                        }, 1000);
+                    } else {
+                        victoryCpt++;
+                        isTimeOutDone = true;
+                        if (victoryCpt == 6) {
+                            setTimeout(() => {
+                                alert(`Bravo ! Vous avez finis en ${score} coups !`);
+                            }, 200);
+                        }
                     }
+                    cardCpt--;
+                    score++; // Increment scoring each 2 card flipped
+                    document.getElementById("currentScore").textContent = `${score}`; // Update the score display
+                } else { // Store the card for next check
+                    storedCard = card;
+                    cardCpt++;
                 }
-                cardCpt--;
-                score++; // Increment scoring each 2 card flipped
-                document.getElementById("currentScore").textContent = `${score}`; // Update the score display
-            } else { // Store the card for next check
-                storedCard = card;
-                cardCpt++;
             }
         });
     });
